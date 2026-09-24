42f708cfaa6369de06a5739a9f10a93fef852a3a
wjy-create <wjy.cherrie@gmail.com>
2026-09-24 20:28:17 +0800

    publish: 2026-09-24 20:28 从 Life.OS/app/ 同步

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Celine Diary service worker：network-first，断网回落缓存。改了 app/ 里任何文件，CACHE 版本号 +1。
-const CACHE='diary-v10';
+const CACHE='diary-v12';
 const ASSETS=['./','diary.html','diary-v2.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
